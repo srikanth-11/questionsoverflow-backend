640e4755936f550cad29083d4e4e871c6cc658bf
srikanth <kasireddysrikanth27@gmail.com>
2021-03-25 16:28:02 +0530

    changed path to the user model

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
-const User = require('../models/User')
+const User = require('../models/user')
 const validator = require('validator');
 const authenticate = require('../services/authentication');
 const MailService = require('../services/mail')
